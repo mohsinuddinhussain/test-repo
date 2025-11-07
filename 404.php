aacf0a17b76409bf78acc9107886fe3b50e4a678
Mohsin <mohsinuddinhussain@gmail.com>
2025-11-07 16:54:39 +0530

    Changes

---
+++ 404.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+<div id="main" class="main" role="main" data-view="view" data-view-name="404">
+
+	<?php the_404_block(); ?>
+
+<?php container_scripts(); ?>
+</div><?php // end #main ?>
+<?php get_footer(); ?>
